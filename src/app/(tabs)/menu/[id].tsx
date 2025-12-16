8b97b61a112e217018a9e736dda3e0da96ee481f
fahimhassanmollah <fahimhassan0197@gmail.com>
2025-12-16 21:26:12 +0600

    Add ProductDetailsScreen and Button component; implement size selection and add to cart functionality

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,71 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
-
+import products from '../../../../assets/data/products';
+import ProductListItem from '@/components/ProductListItem';
+import Button from '@/components/Button';
+const sizes = ['S', 'M', 'L', 'XL', 'XXL'];
 export default function ProductDetailsScreen() {
-    const {id} = useLocalSearchParams();
-    console.log(id);
-    
+  const [selectedSize, setSelectedSize] = React.useState(sizes[0]);
+  const { id } = useLocalSearchParams();
+  const product = products.find(p => p.id.toString() === id);
+  const addToCart = () => {
+    console.log(`Added to cart: ${product?.name}, Size: ${selectedSize}`);
+  }
   return (
-    <View>
-      <Stack.Screen options={{title: `Product ${id}`}} />
-      <Text>ProductDetailsScreen</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image source={{ uri: product?.image }} style={styles.image} />
+      <Text>Select size:</Text>
+      <View style={styles.sizes}>
+        {
+          sizes.map(size => (
+            <Pressable style={[styles.size, selectedSize === size && { backgroundColor: 'gainsboro', }]} key={size} onPress={() => setSelectedSize(size)}>
+              <Text style={[styles.sizeText, selectedSize === size && { fontWeight: '900', color:'gray' }]}>{size}</Text>
+            </Pressable>
+          ))
+        }
+      </View>
+      <Text style={styles.price}>${product?.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
+
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 16,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'contain',
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 8,
+    marginTop: 'auto'
+  },
+  sizes: {
+    flexDirection: 'row',
+    marginVertical: 8,
+    gap: 10,
+    justifyContent: 'space-around',
+  },
+  size: {
+    borderColor: 'gray',
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 16,
+  },
+  sizeText: {
+    fontSize: 18,
+    color: 'black',
+  },
+});
